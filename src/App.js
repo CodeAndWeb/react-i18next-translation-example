e6830ea9715fddbb8099e137c84f8864ad4500b5
Andreas Loew <andreas@codeandweb.com>
2020-06-26 21:15:03 +0200

    Readme (+1 squashed commit) Squashed commits: [4cbc314] Initial checkin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Suspense} from 'react';
 import './App.css';
+import {useTranslation, withTranslation} from "react-i18next";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function HeaderComponent()
+{
+    const [t, i18n] = useTranslation('common');
+    return <div>
+        <h1>{t('welcome.title', {framework:'React'})}</h1>
+        <button onClick={() => i18n.changeLanguage('de')}>de</button>
+        <button onClick={() => i18n.changeLanguage('en')}>en</button>
     </div>
-  );
+}
+
+class HighOrderComponent extends Component {
+    render() {
+        const { t } = this.props;
+        console.log(this.props);
+
+        return (
+            <h1>{t('welcome.title', {framework:'React'})}</h1>
+        )
+    }
+}
+const HighOrderComponentTranslated = withTranslation('common')(HighOrderComponent)
+
+function App()
+{
+    return (
+        <Suspense fallback="loading">
+            <div className="App">
+                <HeaderComponent/>
+                <HighOrderComponentTranslated/>
+            </div>
+        </Suspense>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
+import common_de from "./translations/de/common.json";
+import common_en from "./translations/en/common.json";
+
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+    lng: 'en',                              // language to use
+    resources: {
+        en: {
+            common: common_en               // 'common' is our custom namespace
+        },
+        de: {
+            common: common_de
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+            <App/>
+        </I18nextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
